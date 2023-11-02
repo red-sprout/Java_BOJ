ec08eea20d06e578a516b804cbf4c0a9303cd860
red-sprout <jjw05015@gmail.com>
2023-11-03 00:25:38 +0900

    Linked List - Add header

---
+++ DataStructure/src/LinkedListNode.java
@@ -0,0 +1,56 @@
+class LinkedList {
+    Node header;
+
+    static class Node {
+        int data;
+        Node next = null;
+    }
+
+    LinkedList() {
+        header = new Node();
+    }
+
+    void append(int d) {
+        Node end = new Node();
+        end.data = d;
+        Node n = header;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    void delete(int d) {
+        Node n = header;
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+            } else {
+                n = n.next;
+            }
+        }
+    }
+
+    void retrieve() {
+        Node n = header.next; // header 다음 데이터부터 출력하도록 함.
+        while (n.next != null) {
+            System.out.print(n.data + " -> ");
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+}
+
+public class LinkedListNode {
+    public static void main(String[] args) {
+        LinkedList2 ll = new LinkedList2();
+        ll.append(1);
+        ll.append(2);
+        ll.append(3);
+        ll.append(4);
+        ll.retrieve();
+
+        ll.delete(1); // Node를 감싼 Linked List의 경우 첫번째 Node 삭제 가능
+        ll.retrieve();
+    }
+}
