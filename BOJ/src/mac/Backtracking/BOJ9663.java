61d1491cbee822057c17f3f4f424c784d81b4e0f
red-sprout <jjw05015@gmail.com>
2023-11-11 22:55:16 +0900

    N-queen

--- BOJ/src/mac/Backtracking/BOJ9663.java
+++ BOJ/src/mac/Backtracking/BOJ9663.java
@@ -1,5 +1,10 @@
+// Backtracking N-queen
 package mac.Backtracking;
+import java.io.*;
+import java.util.*;
 
 public class BOJ9663 {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
