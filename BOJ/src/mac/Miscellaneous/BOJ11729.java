9538d19cbc892abb292952444f49f7c1c03ec78e
red-sprout <jjw05015@gmail.com>
2023-12-10 23:42:44 +0900

    재귀

---
+++ BOJ/src/mac/Miscellaneous/BOJ11729.java
@@ -0,0 +1,30 @@
+package mac.Miscellaneous;
+
+import java.util.*;
+
+public class BOJ11729 {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println((int)Math.pow((double)2, (double)N) - 1);
+        hanoi(N, 1, 2, 3);
+        System.out.println(sb);
+        sc.close();
+    }
+    private static void hanoi(int n, int fromPole, int withPole, int toPole) {
+        if (n == 1) {
+            addToString(fromPole, toPole);
+        } else {
+            hanoi(n-1, fromPole, toPole, withPole);
+            addToString(fromPole, toPole);
+            hanoi(n-1, withPole, fromPole, toPole);
+        }
+    }
+    private static void addToString(int fromPole, int toPole) {
+        sb.append(String.valueOf(fromPole));
+        sb.append(" ");
+        sb.append(String.valueOf(toPole));
+        sb.append("\n");
+    }
+}
