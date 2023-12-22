ee2cef7376040634e5ec7d790590702dd154ea8f
red-sprout <jjw05015@gmail.com>
2023-12-22 11:58:26 +0900

    누적 합

--- BOJ/src/mac/Miscellaneous/BOJ10986.java
+++ BOJ/src/mac/Miscellaneous/BOJ10986.java
@@ -5,17 +5,34 @@ import java.util.*;
 
 public class BOJ10986 {
     static int N, M;
-    static int[] A;
+    static long[] R;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        R = new long[M];
+        R[0] = 1;
 
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            A[i] = Integer.parseInt(stk.nextToken());
+        st = new StringTokenizer(br.readLine());
+        long sum = 0;
+        for(int i = 1; i <= N; i++){
+            sum = (sum + Long.parseLong(st.nextToken())) % M;
+            R[(int)sum]++;
         }
+
+        long ans = 0;
+        for(int i = 0; i < M; i++) {
+            ans += select(R[i]);
+        }
+        System.out.println(ans);
+
+        br.close();
+    }
+    private static long select(long n) {
+        if(n < 2) return 0;
+        return n * (n - 1) / 2;
     }
 }
