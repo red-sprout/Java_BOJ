4491411afb509a434d3444d508cb58592f20135b
red-sprout <jjw05015@gmail.com>
2023-12-25 17:34:03 +0900

    진행 중

---
+++ BOJ/src/mac/BinarySearch/BOJ1300.java
@@ -0,0 +1,12 @@
+package mac.BinarySearch;
+
+import java.io.*;
+
+public class BOJ1300 {
+    static int N, k;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+    }
+}
