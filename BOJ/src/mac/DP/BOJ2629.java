4d85c54e8f6786084956564b7691d2aa23b7c2dc
red-sprout <jjw05015@gmail.com>
2024-01-02 11:22:49 +0900

    Dynamic Programming 2D

--- BOJ/src/mac/DP/BOJ2629.java
+++ BOJ/src/mac/DP/BOJ2629.java
@@ -1,41 +1,54 @@
-// 진행중
 package mac.DP;
 
 import java.io.*;
 import java.util.*;
 
 public class BOJ2629 {
-    static int MAX = 40000;
+    static int MAX = 80001;
+    static int ZERO = MAX / 2;
+    // idx == real + 40000
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        boolean[] dp = new boolean[MAX + 1];
 
         int numberOfWeights = Integer.parseInt(br.readLine());
 
         int[] weights = new int[numberOfWeights];
+        boolean[][] dp = new boolean[numberOfWeights + 1][MAX + 1];
+        dp[0][ZERO] = true;
+
         st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < numberOfWeights; i++) {
+        for(int i = 1; i <= numberOfWeights; i++) {
             int weight = Integer.parseInt(st.nextToken());
-            weights[i] = weight;
-        }
+            weights[i - 1] = weight;
 
-        int numberOfBalls = Integer.parseInt(br.readLine());
+            dp[i][ZERO] = true;
+            for(int j = 0; j <= MAX; j++) {
+                if(!dp[i - 1][j]) continue;
 
-        int[] balls = new int[numberOfBalls];
+                dp[i][j] = true;
 
-        for(int i = 1; i <= numberOfBalls; i++) {
-            for(int j = 1; j <= MAX; j++) {
-
+                if(j - weight >= 0) dp[i][j - weight] = true;
+                if(j + weight <= MAX) dp[i][j + weight] = true;
             }
         }
 
+
+        int numberOfBalls = Integer.parseInt(br.readLine());
+
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < numberOfBalls; i++) {
-            balls[i] = Integer.parseInt(st.nextToken());
+            String isPossible = null;
+            int value = Integer.parseInt(st.nextToken());
+
+            if(dp[numberOfWeights][value + ZERO]) isPossible = "Y";
+            else isPossible = "N";
+
+            sb.append(isPossible + " ");
         }
 
+        System.out.println(sb);
         br.close();
     }
 }
