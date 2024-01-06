c87c48b1055af2fc9840b413ab95b2d229fe57d6
red-sprout <jjw05015@gmail.com>
2024-01-06 14:00:58 +0900

    graph

--- BOJ/src/mac/graph/BOJ2206.java
+++ BOJ/src/mac/graph/BOJ2206.java
@@ -6,79 +6,83 @@ import java.util.*;
 
 public class BOJ2206 {
     static class Loc{
-        int i;
-        int j;
+        int row;
+        int col;
         int cnt;
         boolean destroyed;
 
-        public Loc(int i, int j, int cnt, boolean d) {
-            this.i = i;
-            this.j = j;
+        public Loc(int row, int col, int cnt, boolean destroyed) {
+            this.row = row;
+            this.col = col;
             this.cnt = cnt;
-            this.destroyed = d;
+            this.destroyed = destroyed;
         }
     }
 
+    static int N, M;
+    static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
 
-        int n = Integer.parseInt(inputs[0]);
-        int m = Integer.parseInt(inputs[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
 
-        char[][] map = new char[n][m];
-        for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = input.charAt(j);
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
         }
 
-
+        bfs();
+        br.close();
+    }
+    
+    private static void bfs() {
         Queue<Loc> q = new LinkedList<>();
         q.add(new Loc(0, 0, 1, false));
 
-        int[] mi = {0, 0, -1, 1};
-        int[] mj = {-1, 1, 0, 0};
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
 
-        boolean[][][] visited = new boolean[n][m][2];
+        boolean[][][] visited = new boolean[N][M][2];
 
         while (!q.isEmpty()) {
-            Loc now = q.poll();
+            Loc past = q.poll();
 
-            if (now.i == n - 1 && now.j == m - 1) {
-                System.out.println(now.cnt);
+            if (past.row == N - 1 && past.col == M - 1) {
+                System.out.println(past.cnt);
                 return;
             }
 
-            for (int d = 0; d < 4; d++) {
-                int ni = now.i + mi[d];
-                int nj = now.j + mj[d];
+            for (int i = 0; i < 4; i++) {
+                int nowRow = past.row + dr[i];
+                int nowCol = past.col + dc[i];
 
-                if(ni<0 || ni>=n || nj<0 || nj>=m) continue;
+                if(nowRow < 0 || nowRow >= N) continue;
+                if(nowCol < 0 || nowCol >= M) continue;
 
-                int next_cnt = now.cnt+1;
+                int nowCnt = past.cnt+1;
 
-                if(map[ni][nj]=='0'){ // 벽이 아니면
-                    if(!now.destroyed && !visited[ni][nj][0]) { // 부신 벽이 여태까지 없었으면
-                        q.add(new Loc(ni, nj, next_cnt, false));
-                        visited[ni][nj][0] = true;
-                    }else if(now.destroyed && !visited[ni][nj][1]){ // 벽을 한번 부신 적이 있으면
-                        q.add(new Loc(ni, nj, next_cnt, true));
-                        visited[ni][nj][1] = true;
+                if(map[nowRow][nowCol] == 0){
+                    if(!past.destroyed && !visited[nowRow][nowCol][0]) {
+                        q.add(new Loc(nowRow, nowCol , nowCnt, false));
+                        visited[nowRow][nowCol][0] = true;
+                    } else if(past.destroyed && !visited[nowRow][nowCol][1]){
+                        q.add(new Loc(nowRow, nowCol , nowCnt, true));
+                        visited[nowRow][nowCol][1] = true;
                     }
+                    continue;
+                }
 
-                }else if(map[ni][nj]=='1'){ // 벽이면
-
-                    if(!now.destroyed){ // 한번도 벽을 부순적이 없다면 부순다~
-                        q.add(new Loc(ni, nj, next_cnt, true));
-                        visited[ni][nj][1] = true;
-                    }
-                    // 한번 부순 적이 있다면 또 부수고 갈 수 없기 때문에 pass
+                if(!past.destroyed){
+                    q.add(new Loc(nowRow, nowCol , nowCnt, true));
+                    visited[nowRow][nowCol][1] = true;
                 }
             }
-
         }
 
         System.out.println(-1);
