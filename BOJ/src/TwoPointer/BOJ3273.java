c697c1cdd6d347b928fe647da46b1b13c71741dc
red-sprout <jjw05015@gmail.com>
2024-01-08 18:06:12 +0900

    진행 중

---
+++ BOJ/src/TwoPointer/BOJ3273.java
@@ -0,0 +1,10 @@
+package TwoPointer;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
