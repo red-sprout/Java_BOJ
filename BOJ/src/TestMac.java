c9b04ba14a60975eae0595794aa080ad884b107a
red-sprout <jjw05015@gmail.com>
2024-01-08 20:40:09 +0900

    delete

--- BOJ/src/TestMac.java
+++
@@ -1,7 +0,0 @@
-
-
-public class TestMac {
-    public static void main(String[] args) {
-        System.out.println("Hello Mac!");
-    }
-}
--- BOJ/src/xps/TestXps.java
+++
@@ -1,7 +0,0 @@
-package xps;
-
-public class TestXps {
-    public static void main(String[] args) {
-        System.out.println("Hello XPS!");
-    }
-}
